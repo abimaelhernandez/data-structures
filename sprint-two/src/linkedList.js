e175f6dc1550b0b569f402897f078e3d9b777f5b
abimaelhernandez <hernandezabimael364@gmail.com>
2018-01-13 16:44:43 -0600

    done with tree portion

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,13 +4,35 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var nodeData = Node(value)
+    if (list.head === null) {
+      list.head = nodeData
+      list.tail = list.head
+    } else {
+      list.tail.next = nodeData
+      list.tail = list.tail.next
+    }
   };
 
   list.removeHead = function() {
+    if (list.head === null) {
+      return null;
+    }
+    var oldHeads = list.head.value
+    list.head = list.head.next;
+    return oldHeads
   };
 
   list.contains = function(target) {
-  };
+    var theNode = list.head;
+    while (theNode) {
+      if (theNode.value === target) {
+        return true;
+      }
+      theNode = theNode.next;
+    }
+    return false;
+  }
 
   return list;
 };
@@ -23,7 +45,3 @@ var Node = function(value) {
 
   return node;
 };
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,19 +3,39 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //create a child
+  var newChild = new Tree(value);
+  //push into children array
+  this.children.push(newChild);
 };
 
 treeMethods.contains = function(target) {
-};
+  if (this.value === target) {
+    return true;
+  }
 
+  //loop through the children array
+  for (var i = 0; i < this.children.length; i++) {
+    //compare if this value is equal to target
+    var child = this.children[i];
+    if (child.contains(target)) {
+      //return true
+      return true;
+    }
+
+  }
+
+  return false;
+
+};
 
 
 /*
